8bcf78340c7ae019bfe5de4df530aa583a9d319d
wic160023 <wic160023@ANX410.fsktm.edu>
2016-09-07 17:58:43 +0800

    Q6 Top

---
+++ WIC160023/src/Week01/L1Q3.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Week01;
+
+/**
+ *
+ * @author wic160023
+ */
+public class L1Q3 {
+    
+    public static void main(String []arg){
+        System.out.println(".........       ...       .        .");
+        System.out.println(".       .      .   .     ...      . .");
+        System.out.println(".       .     .     .   .....    .   .");
+        System.out.println(".       .    .       .    .     .     .");
+        System.out.println(".       .    .       .    .    .       .");
+        
+        
+    }
+    
+}
