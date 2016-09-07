cae5a0b3309506d4fdb08401a09c442fcf845372
wic160023 <wic160023@ANX410.fsktm.edu>
2016-09-07 18:09:40 +0800

    Q6 finish

---
+++ WIC160023/src/Week01/L1Q4.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Week01;
+
+/**
+ *
+ * @author wic160023
+ */
+public class L1Q4 {
+    public static void main(String []arg){
+        System.out.println("  Table 1; Total Sales of Product A");
+        System.out.println("    |     Month    |    Amount   |");
+        System.out.println("    | January 2015 |     2500    |");
+        System.out.println("    |February 2015 |     1600    |");
+        System.out.println("    |  March 2015  |     2000    |");
+        System.out.println("    |   April 2015 |     2700    |");
+        System.out.println("    |    May 2015  |     3200    |");
+        System.out.println("    |   June 2016  |      800    |");
+    }
+    
+}
