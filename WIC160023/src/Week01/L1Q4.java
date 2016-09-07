63d0e57014bdc793343f2d81bdbc477140ec6949
wic160023 <wic160023@ANX410.fsktm.edu>
2016-09-07 17:36:21 +0800

    Q4

---
+++ WIC160023/src/Week01/L1Q4.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Week01;
+
+/**
+ *
+ * @author wic160023
+ */
+public class L1Q4 {
+    //ccomputeArea
+    /*The purpose of this program is to compute the area where the
+    radius is 20.*/
+    
+    public static void main(String [] args) {
+        int radius;
+        double area;
+        radius = 20;
+        area = radius*radius*3.14159;
+        
+        System.out.println("Area of circle with radius" +radius+ "is" +area);
+    }
+}
