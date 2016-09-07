b4f05807a90ff6650f0c811675e02b9358ea1d40
wic160023 <wic160023@ANX410.fsktm.edu>
2016-09-07 17:27:45 +0800

    Main class

---
+++ WIC160023/src/Week01/L1Q3.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Week01;
+
+/**
+ *
+ * @author wic160023
+ */
+public class L1Q3 {
+    //The purpose of this program isto compute and display the sum of 4
+    //and 32
+    
+    public static void main(String[] args){
+        
+    }
+}
