58e1a570742fae0c497f13cad133db8be0dc12a8
wic160023 <wic160023@ANX410.fsktm.edu>
2016-09-07 17:38:08 +0800

    Q4 finish

--- WIC160023/src/Week01/L1Q4.java
+++ WIC160023/src/Week01/L1Q4.java
@@ -20,6 +20,6 @@ public class L1Q4 {
         radius = 20;
         area = radius*radius*3.14159;
         
-        System.out.println("Area of circle with radius" +radius+ "is" +area);
+        System.out.println("Area of circle with radius " +radius+ " is " +area);
     }
 }
